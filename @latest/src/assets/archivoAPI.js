09d5822e6778394e689746ff0673a9156046fb4f
Darkweaver <albasantiagoe@hotmail.com>
2023-10-19 11:34:53 -0400

    componente 1

---
+++ @latest/src/assets/archivoAPI.js
@@ -0,0 +1,15 @@
+export const user = [
+  {
+    userName: "John",
+    identityUserName: "#929393",
+  },
+
+  {
+    userName: "Arthur",
+    identityUserName: "#634633",
+  },
+  {
+    userName: "Carmen",
+    identityUserName: "#244883",
+  },
+];
